2dee14ab98bf87a510652286e71c845f4462dd88
Roller Angel <techangel428@gmail.com>
2017-07-24 10:22:17 -0600

    Removed measurements as I'm working on a new approach in my concepts.py file

---
+++ concepts.py
@@ -0,0 +1,16 @@
+data = []
+with open('data/bmp180_20160201.dat') as f:
+    for line in f:
+        ndata.append(line.split(" "))
+ndata[2][-1].strip()
+
+flist = []
+for l in ndata:
+    if l[4].endswith('00') or l[4].endswith('15') or l[4].endswith('30') or l[4].endswith('45'):
+        flist.append('DAVAD ZZCOUCAR SITE_ID:32534' + ' ' + l[2]+ l[0]+ l[1]+ l[3]+ l[4]+ ' ' + 'X'+ ' ' + 'X'+ ' ' + 'X'+ ' ' + 'X'+ ' ' + 'X'+ ' ' + 'X'+ ' ' + 'X'+ ' ' + 'X'+ ' ' + 'X'+ ' ' + l[10])
+
+fstring = []
+for l in flist:
+    fstring += l + ' '
+
+data = '//AA ' + fstring + '//ZZ'
